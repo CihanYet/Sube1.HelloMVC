8d3a814e9bf789eefdb5dc786de67512ddab1519
Gazi <Gazi@DESKTOP-224SE1B>
2024-04-15 12:04:38 +0300

    Add project files.

---
+++ Sube1.HelloMVC/Controllers/OgretmenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sube1.HelloMVC.Controllers
+{
+    public class OgretmenController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
